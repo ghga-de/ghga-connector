b7aa4912454aaeb0171c6198a8a1c3b56dec8813
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2022-05-13 12:25:55 +0000

    Remove old test

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -21,7 +21,7 @@ from os import path, remove
 
 import pytest
 
-from ghga_connector.core import check_url, download_file_part, upload_file
+from ghga_connector.core import check_url, download_file_part
 
 from ..fixtures import s3_fixture  # noqa: F401
 from ..fixtures import state
@@ -50,29 +50,6 @@ def test_check_url(api_url, wait_time, expected_response):
     assert response == expected_response
 
 
-def test_upload_file(
-    s3_fixture,  # noqa F811
-):
-    """
-    Test the upload_url function
-    """
-
-    uploadable_file = state.FILES["file_uploadable"]
-    presigned_post = s3_fixture.storage.get_object_upload_url(
-        bucket_id=uploadable_file.grouping_label,
-        object_id=uploadable_file.file_id,
-        expires_after=60,
-    )
-    upload_file(
-        presigned_post=presigned_post,
-        upload_file_path=str(uploadable_file.file_path.resolve()),
-    )
-    assert s3_fixture.storage.does_object_exist(
-        bucket_id=uploadable_file.grouping_label,
-        object_id=uploadable_file.file_id,
-    )
-
-
 def test_download_file(
     s3_fixture,  # noqa F811
 ):
@@ -89,10 +66,11 @@ def test_download_file(
 
     # Try to download the whole test file in one part. Should be fairly small.
     download_file_part(
-        download_url,
-        EXAMPLE_File,
+        download_url=download_url,
+        output_file_path=EXAMPLE_File,
         part_offset=0,
-        part_end=path.getsize(downloadable_file.file_path) - 1,
+        part_size=path.getsize(downloadable_file.file_path),
+        file_size=path.getsize(downloadable_file.file_path),
     )
 
     assert cmp(EXAMPLE_File, downloadable_file.file_path)

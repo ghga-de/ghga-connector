a050cff01d6c84f1083c388113de0557f03117fa
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2022-05-30 11:13:21 +0200

    Revert "check_url now expects correct exception"
    This reverts commit 088cb9622a4fbe9684b67ab491fa4ba0c3651da5.

--- ghga_connector/core/main.py
+++ ghga_connector/core/main.py
@@ -26,6 +26,6 @@ def check_url(api_url, wait_time=1000) -> bool:
     try:
         # timeout takes seconds, was ms in curl, convert accordingly
         requests.get(url=api_url, timeout=wait_time / 1000)
-    except requests.exceptions.Timeout:
+    except requests.exceptions.RequestException:
         return False
     return True

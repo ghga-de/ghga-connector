7bf44a169bf037d24919433c3493c5568cddbd31
Moritz Hahn <moritz.hahn@uni-tuebingen.de>
2023-04-25 10:50:51 +0000

    Seperated API calls into domains

--- ghga_connector/core/api_calls/upload.py
+++ ghga_connector/core/api_calls/upload.py
@@ -12,28 +12,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 """
-Contains calls to the GHGA storage API
+This file contains all api calls related to uploading files
 """
 
 import base64
 import json
-from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
-from time import sleep
-from typing import Dict, Iterator, Tuple, Union
+from typing import Dict, Iterator, Tuple
 
 import crypt4gh.keys
 import requests
-from requests.structures import CaseInsensitiveDict
 
 from ghga_connector.core import exceptions
 from ghga_connector.core.constants import MAX_PART_NUMBER, TIMEOUT
 from ghga_connector.core.http_translation import ResponseExceptionTranslator
-from ghga_connector.core.message_display import AbstractMessageDisplay
 from ghga_connector.core.session import RequestsSession
 
 # Constants for clarity of return values
@@ -53,30 +48,6 @@ class UploadStatus(str, Enum):
     PENDING = "pending"
     REJECTED = "rejected"
     UPLOADED = "uploaded"
-
-
-@dataclass
-class WPSInfo:
-    """
-    Container for WPS endpoint information
-    """
-
-    file_ids_with_extension: dict[str, str]
-    ghga_pubkey: bytes
-    user_id: str
-    user_pubkey: bytes
-
-
-def check_url(api_url, *, wait_time=1000) -> bool:
-    """
-    Checks, if an url is reachable within a certain time
-    """
-    try:
-        # timeout takes seconds, was ms in curl, convert accordingly
-        requests.get(url=api_url, timeout=wait_time / 1000)
-    except requests.exceptions.RequestException:
-        return False
-    return True
 
 
 def initiate_multipart_upload(
@@ -323,83 +294,6 @@ def get_file_metadata(*, api_url: str, file_id: str) -> Dict:
     return file_metadata
 
 
-def get_download_url(
-    *,
-    api_url: str,
-    file_id: str,
-) -> Union[Tuple[None, None, int], Tuple[str, int, None]]:
-    """
-    Perform a RESTful API call to retrieve a presigned download URL.
-    Returns:
-        A tuple of three elements:
-            1. the download url
-            2. the file size (in bytes)
-            3. the retry-time
-        If the download url is not available yet, the first two elements are None and
-        the retry-time is set.
-        Otherwise, only the last element is None while the others are set.
-    """
-
-    # build url and headers
-    url = f"{api_url}/objects/{file_id}"
-
-    headers = CaseInsensitiveDict(
-        {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        }
-    )
-
-    # Make function call to get download url
-    try:
-        response = RequestsSession.get(url=url, headers=headers, timeout=TIMEOUT)
-    except requests.exceptions.RequestException as request_error:
-        exceptions.raise_if_max_retries(request_error=request_error, url=url)
-        raise exceptions.RequestFailedError(url=url) from request_error
-
-    status_code = response.status_code
-    if status_code != 200:
-        if status_code != 202:
-            raise exceptions.BadResponseCodeError(url=url, response_code=status_code)
-
-        headers = response.headers
-        if "retry-after" not in headers:
-            raise exceptions.RetryTimeExpectedError(url=url)
-
-        return (NO_DOWNLOAD_URL, NO_FILE_SIZE, int(headers["retry-after"]))
-
-    # look for an access method of type s3 in the response:
-    response_body = response.json()
-    download_url = None
-    access_methods = response_body["access_methods"]
-    for access_method in access_methods:
-        if access_method["type"] == "s3":
-            download_url = access_method["access_url"]["url"]
-            file_size = response_body["size"]
-            break
-
-    if download_url is None:
-        raise exceptions.NoS3AccessMethodError(url=url)
-
-    return download_url, file_size, NO_RETRY_TIME
-
-
-def get_download_urls(
-    *,
-    api_url: str,
-    file_id: str,
-) -> Iterator[Union[Tuple[None, None, int], Tuple[str, int, None]]]:
-    """
-    For a specific mutli-part upload identified by the `file_id`, it returns an
-    iterator to obtain download_urls.
-    """
-    while True:
-        yield get_download_url(
-            api_url=api_url,
-            file_id=file_id,
-        )
-
-
 def start_multipart_upload(
     *, api_url: str, file_id: str, pubkey_path: Path
 ) -> Tuple[str, int]:
@@ -433,112 +327,3 @@ def start_multipart_upload(
         raise error
 
     return multipart_upload
-
-
-def await_download_url(
-    *,
-    api_url: str,
-    file_id: str,
-    max_wait_time: int,
-    message_display: AbstractMessageDisplay,
-) -> Tuple[str, int]:
-    """Wait until download URL can be generated.
-    Returns a tuple with two elements:
-        1. the download url
-        2. the file size in bytes
-    """
-
-    # get the download_url, wait if needed
-    wait_time = 0
-    while wait_time < max_wait_time:
-        try:
-            response_body = get_download_url(
-                api_url=api_url,
-                file_id=file_id,
-            )
-        except exceptions.BadResponseCodeError as error:
-            message_display.failure(
-                "The request was invalid and returnd a wrong HTTP status code."
-            )
-            raise error
-        except exceptions.RequestFailedError as error:
-            message_display.failure("The request has failed.")
-            raise error
-
-        if response_body[0] is not None:
-            download_url: str = response_body[0]
-            file_size: int = response_body[1]
-            return (download_url, file_size)
-
-        retry_time: int = response_body[2]
-
-        wait_time += retry_time
-        message_display.display(
-            f"File staging, will try to download again in {retry_time} seconds"
-        )
-        sleep(retry_time)
-
-    raise exceptions.MaxWaitTimeExceededError(max_wait_time=max_wait_time)
-
-
-def get_file_header_envelope(file_id: str, api_url: str, public_key: bytes) -> bytes:
-    """
-    Perform a RESTful API call to retrieve a file header envelope.
-    Returns:
-        The file header envelope (bytes object)
-    """
-
-    # encode public key in base64 (url-safe)
-    public_key_encoded = base64.urlsafe_b64encode(public_key).decode("utf-8")
-
-    # build url and headers
-    url = f"{api_url}/objects/{file_id}/envelopes/{public_key_encoded}"
-
-    headers = CaseInsensitiveDict(
-        {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        }
-    )
-
-    # Make function call to get download url
-    try:
-        response = RequestsSession.get(url=url, headers=headers, timeout=TIMEOUT)
-    except requests.exceptions.RequestException as request_error:
-        raise exceptions.RequestFailedError(url=url) from request_error
-
-    status_code = response.status_code
-
-    if status_code == 200:
-        return base64.b64decode(response.content)
-    spec = {
-        404: {
-            "envelopeNotFoundError": lambda: exceptions.EnvelopeNotFoundError(
-                file_id=file_id
-            ),
-            "noSuchObject": lambda: exceptions.FileNotRegisteredError(file_id=file_id),
-        },
-        500: {"externalAPIError": exceptions.ExternalApiError},
-    }
-
-    ResponseExceptionTranslator(spec=spec).handle(response=response)
-    raise exceptions.BadResponseCodeError(url=url, response_code=status_code)
-
-
-def get_wps_info(config):
-    """
-    Call WPS endpoint and retrieve necessary information.
-    For now, mock the call and return information from config.
-    """
-    ghga_pubkey = base64.b64decode(config.server_pubkey)
-    user_pubkey = base64.b64decode(config.wps_user_pubkey)
-
-    file_ids_with_ending = dict(zip(config.wps_file_list, config.wps_file_endings))
-
-    wps_info = WPSInfo(
-        file_ids_with_extension=file_ids_with_ending,
-        ghga_pubkey=ghga_pubkey,
-        user_id=config.wps_user_id,
-        user_pubkey=user_pubkey,
-    )
-    return wps_info

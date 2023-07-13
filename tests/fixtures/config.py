ea5eed73c00004e203f8a27e820867cd0ce0c3de
TheByronHimes <TheByronHimes@gmail.com>
2023-07-13 07:29:53 +0000

    Remove unused API urls from test config

--- tests/fixtures/config.py
+++ tests/fixtures/config.py
@@ -20,13 +20,10 @@ from ghga_connector import core
 from ghga_connector.config import Config
 
 DEFAULT_TEST_CONFIG = Config(
-    upload_api="http:/example.org/upload",
-    download_api="http:/example.org/download",
     max_retries=0,
     max_wait_time=2,
     part_size=core.DEFAULT_PART_SIZE,
     wkvs_api_url="https://127.0.0.1",
-    wps_api_url="http://127.0.0.1",
 )
 
 

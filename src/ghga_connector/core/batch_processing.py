73af30f36c2f78545d11d5f6d0231b6702e0bdbb
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-12-14 10:02:32 +0000

    Fix typo

--- src/ghga_connector/core/batch_processing.py
+++ src/ghga_connector/core/batch_processing.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Module for btach processing specific code"""
+"""Module for batch processing specific code"""
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field

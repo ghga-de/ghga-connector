3b2a84be55df28382e4a267067f2cfc60baa6c4f
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-12-14 09:51:13 +0000

    Remove black from pre-commit

--- src/ghga_connector/core/api_calls/work_package.py
+++ src/ghga_connector/core/api_calls/work_package.py
@@ -99,9 +99,7 @@ class WorkPackageAccessor:
                 base64.b64decode(token.split(".", 2)[1]).decode("utf-8")
             )["user_public_crypt4gh_key"] != base64.b64encode(
                 self.my_public_key
-            ).decode(
-                "ascii"
-            )
+            ).decode("ascii")
         except Exception:  # pylint: disable=broad-exception-caught
             mismatch = False
         if mismatch:

0bb8ce4eaf76ae5f70ac566157bc400db2c5cb42
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2024-01-15 09:07:12 +0000

    Added dummy docstrings

--- src/ghga_connector/core/downloading/downloader.py
+++ src/ghga_connector/core/downloading/downloader.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""TODO"""
 
 import base64
 import concurrent.futures
--- src/ghga_connector/core/uploading/abstract_uploader.py
+++ src/ghga_connector/core/uploading/abstract_uploader.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""TODO"""
+
 from abc import ABC, abstractmethod
 from collections.abc import Iterator
 

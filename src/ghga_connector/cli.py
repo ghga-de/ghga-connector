b592ce98b889bb0cb4d749b21420d832d8823936
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-02-25 16:21:37 +0100

    Update src/ghga_connector/cli.py
    Co-authored-by: Byron Himes <thebyronhimes@gmail.com>

--- src/ghga_connector/cli.py
+++ src/ghga_connector/cli.py
@@ -197,7 +197,7 @@ def upload(  # noqa: PLR0913
     ),
     passphrase: Optional[str] = typer.Option(
         None,
-        help="Passphrasse for the encrypted private key. "
+        help="Passphrase for the encrypted private key. "
         + "Only needs to be provided if the key is actually encrypted.",
     ),
     debug: bool = typer.Option(

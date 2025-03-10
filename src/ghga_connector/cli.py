5f7186323e9235ef6fabed7a2e415fbd3b5eed49
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-03-10 13:01:54 +0000

    Hishel debug log

--- src/ghga_connector/cli.py
+++ src/ghga_connector/cli.py
@@ -305,6 +305,7 @@ async def async_download(  # noqa: PLR0913
     # enable debug logging
     if debug:
         logging.getLogger().setLevel(logging.DEBUG)
+        logging.getLogger("hishel.controller").setLevel(logging.DEBUG)
 
     if not my_public_key_path.is_file():
         raise exceptions.PubKeyFileDoesNotExistError(public_key_path=my_public_key_path)

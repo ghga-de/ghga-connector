fc21a6f41e88b0d79f7647d992f69a460aa59b5d
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-11-05 09:28:51 +0000

    Custom user agent

--- src/ghga_connector/core/client.py
+++ src/ghga_connector/core/client.py
@@ -15,7 +15,6 @@
 """Handling session initialization for httpx"""
 
 from contextlib import asynccontextmanager
-from typing import Union
 
 import hishel
 import httpx
@@ -58,7 +57,7 @@ class RetryHandler:
 
 
 def get_cache_transport(
-    wrapped_transport: Union[httpx.AsyncBaseTransport, None] = None,
+    wrapped_transport: httpx.AsyncBaseTransport | None = None,
 ) -> hishel.AsyncCacheTransport:
     """Construct an async cache transport with `hishel`.
 
@@ -88,6 +87,7 @@ def get_mounts() -> dict[str, httpx.AsyncBaseTransport]:
 async def async_client():
     """Yields a context manager async httpx client and closes it afterward"""
     async with httpx.AsyncClient(
+        headers=httpx.Headers({"User-Agent": CONFIG.user_agent}),
         timeout=TIMEOUT,
         limits=httpx.Limits(
             max_connections=CONFIG.max_concurrent_downloads,
